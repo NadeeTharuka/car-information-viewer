52ae5223dbeabbf633c1ce53f1f4ca1d504f6420
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-04-27 20:09:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI,Query,Path, HTTPException, status, Body
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
 from database import cars
@@ -30,3 +30,28 @@ def get_cars(number: Optional[str] = Query("10",max_length=3)):
         to_add[id] = car
         response.append(to_add)
     return response
+
+
+@app.get("/cars/{id}", response_model=Car)
+def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+    car = cars.get(id)
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID.")
+    
+    return car
+
+@app.post("/cars", status_code=status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+    if len(body_cars) < 1:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
+        min_id = len(cars.values()) + min_id
+        for car in body_cars:
+            while cars.get(min_id):
+                min_id += 1
+            cars[min_id] = car
+            min_id += 1
+
+
+
+
+
